7f012cc484e77650f6a41f62a7c19e4863a042b2
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2017-11-20 18:06:49 +0530

    HomeController obj added, switchsections added with handler

--- mowgli-ix/js/main.js
+++ mowgli-ix/js/main.js
@@ -1,6 +1,62 @@
 var scrollContainer = document.getElementById("home-scroll");
-var current = 0,
-    prev = null,
-    next = 1;
+var currSc = 0,
+    prevSc = null,
+    nextSc = 1,
+    dir = 0,
+    homeSections = document.getElementsByClassName("row-home");
 
-console.log(next);
+console.log(homeSections);
+
+//Home Scroll Controller object
+function HomeController(el) {
+    this.el = el;
+    this.el.addEventListener('wheel', this, false);
+    // this.init;
+}
+
+// HomeController methods
+HomeController.prototype.handleEvent = function(e) {
+    e.stopPropagation();
+    if (e.type === "wheel") {
+        //get direction of scroll
+        if (e.deltaY > 0) {
+            dir = 1;
+
+            this.switchHomeNext(currSc);
+        }
+        else {
+            dir = -1;
+            this.switchHomePrev(currSc);
+        }
+    }
+    
+}
+
+HomeController.prototype.switchHomeNext = function(current) {
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    currSc++;
+    // nextSc++;
+
+
+    var pageTr = anime({
+        targets: scrollContainer,
+        top: -(h*currSc),
+        easing: 'easeInOutQuad'
+    })
+    //console.log('scroll down')
+}
+
+HomeController.prototype.switchHomePrev = function(current) {
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    currSc--;
+    var pageTr = anime({
+        targets: scrollContainer,
+        top: -(h*currSc),
+        easing: 'easeInOutQuad'
+    })
+}
+
+
+
+//let object go freeeeee :)
+var homeObj = new HomeController(scrollContainer);
