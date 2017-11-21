8d8fe7ef6b10293650d3190e05a99512a22c1cf5
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2017-11-21 13:51:30 +0530

    debounce added to eventhandler

--- mowgli-ix/js/main.js
+++ mowgli-ix/js/main.js
@@ -1,4 +1,16 @@
+//vars
 var scrollContainer = document.getElementById("home-scroll");
+var homeIntro = document.getElementById("home-intro");
+var homeDest = document.getElementById("home-dest");
+var homeTours = document.getElementById("home-tours");
+var homeBlog = document.getElementById("home-blog");
+
+//scroll monitor watchers
+var introWatcher = scrollMonitor.create(homeIntro),
+    destWatcher = scrollMonitor.create(homeDest),
+    toursWatcher = scrollMonitor.create(homeTours),
+    blogWatcher = scrollMonitor.create(homeBlog);
+
 var currSc = 0,
     prevSc = null,
     nextSc = 1,
@@ -6,6 +18,22 @@ var currSc = 0,
     homeSections = document.getElementsByClassName("row-home");
 
 console.log(homeSections);
+
+//debounce function ( - David Walsh Blog)
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
 
 //Home Scroll Controller object
 function HomeController(el) {
@@ -15,24 +43,31 @@ function HomeController(el) {
 }
 
 // HomeController methods
-HomeController.prototype.handleEvent = function(e) {
+
+//handle events, add switch-case for touch/swipe/click detect
+//pass immediate as "true"/"false" to trigger event on the leading or trailing end
+HomeController.prototype.handleEvent = debounce(function(e) {
     e.stopPropagation();
     if (e.type === "wheel") {
         //get direction of scroll
         if (e.deltaY > 0) {
             dir = 1;
-
+            console.log('wheeldown')
             this.switchHomeNext(currSc);
         }
         else {
+            console.log('wheelup');
             dir = -1;
             this.switchHomePrev(currSc);
         }
     }
     
-}
+}, 500, true);
 
 HomeController.prototype.switchHomeNext = function(current) {
+    if (currSc === homeSections.length - 1) {
+        return;
+    }
     var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     currSc++;
     // nextSc++;
@@ -47,6 +82,9 @@ HomeController.prototype.switchHomeNext = function(current) {
 }
 
 HomeController.prototype.switchHomePrev = function(current) {
+    if (currSc === 0) {
+        return;
+    }
     var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     currSc--;
     var pageTr = anime({
