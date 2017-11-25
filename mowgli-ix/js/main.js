9071b32b13ada8e765de5895afb6a73037ac2fae
Scientist94 <shivam.bhattacharjee94@gmail.com>
2017-11-25 21:00:12 +0530

    localhome changes, disregard

--- mowgli-ix/js/main.js
+++ mowgli-ix/js/main.js
@@ -1,10 +1,9 @@
-//vars
-var menuEl = document.querySelector('.nav-menu');
-var scrollContainer = document.getElementById("home-scroll");
-var homeIntro = document.getElementById("home-intro");
-var homeDest = document.getElementById("home-dest");
-var homeTours = document.getElementById("home-tours");
-var homeBlog = document.getElementById("home-blog");
+// //vars
+// var scrollContainer = document.getElementById("home-scroll");
+// var homeIntro = document.getElementById("home-intro");
+// var homeDest = document.getElementById("home-dest");
+// var homeTours = document.getElementById("home-tours");
+// var homeBlog = document.getElementById("home-blog");
 
 // //scroll monitor watchers
 // var introWatcher = scrollMonitor.create(homeIntro),
@@ -20,21 +19,21 @@ var homeBlog = document.getElementById("home-blog");
 
 // console.log(homeSections);
 
-//debounce function ( - David Walsh Blog)
-function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
+// //debounce function ( - David Walsh Blog)
+// function debounce(func, wait, immediate) {
+// 	var timeout;
+// 	return function() {
+// 		var context = this, args = arguments;
+// 		var later = function() {
+// 			timeout = null;
+// 			if (!immediate) func.apply(context, args);
+// 		};
+// 		var callNow = immediate && !timeout;
+// 		clearTimeout(timeout);
+// 		timeout = setTimeout(later, wait);
+// 		if (callNow) func.apply(context, args);
+// 	};
+// };
 
 // //Home Scroll Controller object
 // function HomeController(el) {
@@ -104,116 +103,8 @@ function debounce(func, wait, immediate) {
 //     })
 // }
 
+
+
 // //let object go freeeeee :)
 // var homeObj = new HomeController(scrollContainer);
 
-//menu controller 
-function MenuController(el) {
-    this.el = el;
-    this.isOpen = false;
-    this.isAnimating = false;
-    this.el.addEventListener("click", this, false);
-    //this.init;
-}
-
-// MenuController.prototype.init = function() {
-    
-// }
-
-MenuController.prototype.handleEvent = function(e) {
-    console.log('menu click');
-    //in-icon animation to register click
-
-    //menu-slide down, menu-elements transition in
-    this.menuAnim();
-}
-
-//menu icon transition
-MenuController.prototype.menuIconAnim = function() {
-    
-}
-
-
-//menu screen transition
-
-MenuController.prototype.menuAnim = function() {
-    
-    //menu text switch
-    var menuText = document.querySelector(".burger-text-menu");
-    this.switchMenuText(menuText);
-    //anime timeline
-
-    // var menuTl = anime.timeline();
-    // menuTl.add({
-
-    // })
-    var pageTr = anime({
-        targets: menuEl,
-        opacity: 1,
-        top: ["-100vh", "0"],
-        easing: 'easeInOutCirc',
-        duration: 900
-    });
-    
-}
-
-MenuController.prototype.switchMenuText = function(menuEl) {
-
-    var closeText = "Close";
-    var openText = "Menu";    
-
-    if (this.isOpen == false) {
-       var menuOpen = anime({
-            targets: menuEl,
-            left: "35%",
-            opacity: 0,
-            duration: 800,
-            easing: "easeOutQuad"
-        });
-       setTimeout(function() {
-          menuEl.innerHTML = closeText;
-            menuEl.style.left = "-35%";
-
-            var menuClose = anime({
-                targets: menuEl,
-                left: 0,
-                opacity: 1,
-                duration: 800,
-                easing: "easeOutQuad"
-            });  
-       }, 600);
-        
-        this.isOpen = true;
-    }
-    else {
-        var menuOpen = anime({
-            targets: menuEl,
-            left: "35%",
-            opacity: 0,
-            duration: 800,
-            easing: "easeOutQuad"
-        });
-
-        setTimeout(function() {
-            menuEl.innerHTML = openText;
-            menuEl.style.left = "-35%";
-
-            var menuClose = anime({
-                targets: menuEl,
-                left: 0,
-                opacity: 1,
-                duration: 800,
-                easing: "easeOutQuad"
-            });  
-        }, 600);
-        
-
-        this.isOpen = false;
-
-    }
-
-    
-
-};
-
-var menuInit = new MenuController(document.getElementById("nav-burger"));
