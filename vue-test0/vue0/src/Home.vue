8e731f530485e1a1ea332a2e1f6a6a788412ceb3
ShivamBh <shivam.bhattacharjee94@gmail.com>
2017-12-02 12:24:18 +0530

    vue-js-tuts components primer init

---
+++ vue-test0/vue0/src/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p>Staus: {{ status }}</p>
+        <hr>
+        <button @click="changeStatus">Change</button>
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                status: 'Critical'
+            }
+        },
+        methods: {
+            changeStatus() {
+                this.status = 'Dead'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
